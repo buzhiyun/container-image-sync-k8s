c88811d6c5654aa71262452c3cae33c838b54d68
buzhiyun <buzhiyun@sina.com>
2025-01-13 08:50:31 +0800

    debug kubeadm

--- start.sh
+++ start.sh
@@ -4,12 +4,14 @@
 # RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
 ARCH="amd64"
 
-sudo curl -L --remote-name-all https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/kubeadm
-sudo chmod +x kubeadm
+echo "https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/kubeadm"
+
+curl -L --remote-name-all https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/kubeadm
+chmod +x kubeadm
 
 ls -la
 
-./kubeadm config images list -o text --skip-headers --kubernetes-version ${RELEASE}
+./kubeadm config images list -o text --skip-headers --kubernetes-version "${RELEASE}"
 
 k8s_image_list=$(./kubeadm config images list -o text --skip-headers --kubernetes-version ${RELEASE})
 # 定义重点镜像的关键词列表
